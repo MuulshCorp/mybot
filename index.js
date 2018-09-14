9f5ee3c3393a3b6a429ec535592082fb0db3da54
Slacker Company <38662305+MuulshCorp@users.noreply.github.com>
2018-09-14 23:32:10 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
 var request = require("request");
-const config = require("config.json");
+const config = require("./config.json");
 
 
 client.on("ready", () => {
